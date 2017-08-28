cfc744adf28f73361915b402dd7611683de2f2f8
Ian Harris <ianemailaddress@gmail.com>
2017-08-27 22:16:50 -0700

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Perhaps the simplest app in the world, all this does is allow you to add links to a list.<br />
+      Try adding some links below :)
+    </p>
+    <div>
+      <div>
+        <span>URL:</span>
+        <input id='urlInput' v-on:keypress.enter='addThing'/>
+      </div>
+      <div>
+        <span>Label:</span>
+        <input id='textInput' v-on:keypress.enter='addThing'/>
+      </div>
+      <button v-on:click='addThing'>Add Link</button>
+    </div>
+    <h2>List of links:</h2>
+    <ul>
+      <li v-for='(item, index) in items'>
+        <a :href='item.url' target="_blank">{{ item.text }}</a>
+        <button v-on:click='deleteThing(index)'>X</button>
+      </li>
+    </ul>
+    <child />
+  </div>
+</template>
+
+<script>
+const Child = {
+  template: '<div>A child component</div>',
+};
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Practice Vue App',
+      items: [],
+    };
+  },
+  methods: {
+    addThing() {
+      const urlInput = document.getElementById('urlInput');
+      const textInput = document.getElementById('textInput');
+      if (urlInput.value !== '' || textInput.value !== '') {
+        this.items.push({
+          url: urlInput.value,
+          text: textInput.value,
+        });
+        urlInput.value = '';
+        textInput.value = '';
+      }
+    },
+    deleteThing(index) {
+      debugger;
+      this.items.splice(index, 1);
+    },
+  },
+  components: {
+    child: Child,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Turtles.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='turles'>
+    <h1>I like turtles</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Turtles',
+  data() {
+    return {
+      msg: 'I like turtles'
+    },
+  }
+};
+</script>
